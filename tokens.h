2e85561ee784ea85152b28bd0d170ed90edf7e4d
mira <miraslamaj@gmail.com>
2023-08-29 23:34:28 +0300

    lexical analysis

---
+++ tokens.h
@@ -0,0 +1,32 @@
+#define T_EOF           0
+#define LAYOUT_1        1
+#define LAYOUT_2        2
+#define RGROUP          3
+#define TEXTVIEW        4
+#define IMAGEVIEW       5
+#define BUTTON          6
+#define RBUTTON         7
+#define PROGRESSBAR     8
+
+#define ANDROIDTAG      9
+#define WIDTH           10
+#define HEIGHT          11
+#define ID              12
+#define ORIENTATION     13
+#define TEXT            14
+#define TEXTCOLOR       15
+#define SOURCE          16
+#define PADDING         17
+#define CHECK_B         18
+#define MAX             19
+#define PROGRESS        20
+
+#define START_TAG       21
+#define SMALL_CLOSETAG  22
+#define ENDTAG          23
+#define CLOSETAG        24
+#define ASSIGN          25
+
+#define ST_VALUE        26
+#define INT             27
+#define T_COMMENT       28
